3ffcf846fbe6d82ba33aaf33a914698bec037759
Mark Lennox <mark@webpusher.ie>
2018-03-28 07:31:06 +0100

    passing basic tests for voice stealing

--- common/voiceManager.test.js
+++ common/voiceManager.test.js
@@ -18,6 +18,20 @@ describe("voice manager", () => {
         steal: () => {},
         noteOff: () => {},
         noteOn: (note, velocity) => {}
+      },
+      {
+        oscillator: () => {},
+        volume: () => {},
+        steal: () => {},
+        noteOff: () => {},
+        noteOn: (note, velocity) => {}
+      },
+      {
+        oscillator: () => {},
+        volume: () => {},
+        steal: () => {},
+        noteOff: () => {},
+        noteOn: (note, velocity) => {}
       }
     ];
   });
@@ -35,20 +49,41 @@ describe("voice manager", () => {
 
   describe("assign voices", () => {
     it("no voices sounding new voice assigned to lowest available", () => {
-      const stackAfter = [{ note: 10, time: 1000 }, undefined];
-      vm.voice_index_free = vm.getNextFree();
+      const stackAfter = [
+        { note: 10, time: 1000 },
+        undefined,
+        undefined,
+        undefined
+      ];
+      const expected_free_index = 1;
+      vm.voice_index_free = vm.getNextFree(1000);
+
       vm.note_AddOrUpdate(10, 100);
+
       expect(vm.voice_stack).toEqual(stackAfter);
+      expect(vm.voice_index_free).toEqual(expected_free_index);
+      expect(vm.voice_memo).toEqual({ "10": 0 });
     });
 
     it("one voice free, new voice assigned to free slot", () => {
-      const stackBefore = [null, { note: 9, time: 900 }];
-      const stackAfter = [{ note: 10, time: 1000 }, { note: 9, time: 900 }];
+      const stackBefore = [
+        null,
+        { note: 9, time: 900 },
+        { note: 12, time: 910 },
+        { note: 34, time: 800 }
+      ];
+      const stackAfter = [
+        { note: 10, time: 1000 },
+        { note: 9, time: 900 },
+        { note: 12, time: 910 },
+        { note: 34, time: 800 }
+      ];
+      const expected_free_index = 3;
       vm.voice_stack = stackBefore;
-      vm.voice_index_free = vm.getNextFree();
+      vm.voice_index_free = vm.getNextFree(1000);
       vm.note_AddOrUpdate(10, 100);
       expect(vm.voice_stack).toEqual(stackAfter);
-      expect(vm.voice_index_free).toEqual(1);
+      expect(vm.voice_index_free).toEqual(expected_free_index);
       expect(vm.voice_memo).toEqual({ "10": 0 });
     });
 
@@ -65,16 +100,42 @@ describe("voice manager", () => {
         { note: 19, time: 900 },
         { note: 10, time: 1000 }
       ];
+      const expected_free_index = 0;
       vm.voice_stack = stackBefore;
-      vm.voice_index_free = vm.getNextFree();
+      vm.voice_index_free = vm.getNextFree(1000);
+
       vm.note_AddOrUpdate(10, 100);
+
       expect(vm.voice_stack).toEqual(stackAfter);
-      expect(vm.voice_index_free).toEqual(0);
+      expect(vm.voice_index_free).toEqual(expected_free_index);
       expect(vm.voice_memo).toEqual({ "10": 3 });
     });
 
     it("all voices sounding, a few new voices added in correct places", () => {
-      // todo
+      const stackBefore = [
+        { note: 12, time: 800 },
+        { note: 20, time: 910 },
+        { note: 19, time: 900 },
+        { note: 15, time: 760 }
+      ];
+      const stackAfter = [
+        { note: 12, time: 1001 },
+        { note: 30, time: 1003 },
+        { note: 15, time: 1002 },
+        { note: 10, time: 1000 }
+      ];
+      const expected_free_index = 3;
+      vm.voice_stack = stackBefore;
+      vm.voice_index_free = vm.getNextFree(1000);
+
+      vm.note_AddOrUpdate(10, 100);
+      vm.note_AddOrUpdate(12, 100);
+      vm.note_AddOrUpdate(15, 100);
+      vm.note_AddOrUpdate(30, 100);
+
+      expect(vm.voice_stack).toEqual(stackAfter);
+      expect(vm.voice_index_free).toEqual(expected_free_index);
+      expect(vm.voice_memo).toEqual({ "10": 3, "12": 0, "15": 2, "30": 1 });
     });
   });
 
