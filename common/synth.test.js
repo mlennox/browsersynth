fbb4cf09dbd8ea5bf2edab149920e0b9aac4e1b8
Mark Lennox <mark@webpusher.ie>
2018-03-31 22:00:22 +0100

    stubbing the fixture mocks

--- common/synth.test.js
+++ common/synth.test.js
@@ -6,7 +6,21 @@ describe("synth", () => {
       expect(Synth).toThrow();
     });
   });
-  describe("init", () => {
+
+  describe("others", () => {
+    let synth, synthVoice_mock, ctx_mock;
+
+    beforeEach(() => {
+      synthVoice_mock = {};
+      ctx_mock = {
+        timestamp: 999,
+        get currentTime() {
+          this.timestamp++;
+          return this.timestamp;
+        }
+      };
+      synth = new Synth({ synthVoice: synthVoice_mock, ctx: ctx_mock });
+    });
     it("works", () => {});
   });
 });
