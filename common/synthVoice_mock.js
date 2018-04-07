26eb19e0ff3cc1978f04c1a3894e01c7019b2de6
Mark Lennox <mark@webpusher.ie>
2018-04-07 14:15:57 +0100

    fleshing out the synthVoice mock

--- common/synthVoice_mock.js
+++ common/synthVoice_mock.js
@@ -1,3 +1,17 @@
 function synthVoice_mock() {}
 
+synthVoice_mock.prototype = {
+  init: () => {},
+  calculateVelocity: () => {},
+  midiNoteToFrequency: () => {},
+  generate: () => {
+    return {
+      noteOn: () => {},
+      noteOff: () => {},
+      steal: () => {},
+      polyPress: () => {}
+    };
+  }
+};
+
 export { synthVoice_mock };
