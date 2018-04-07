887d30613ca9caceadd4059d63a1a9937ab307ff
Mark Lennox <mark@webpusher.ie>
2018-04-07 14:38:47 +0100

    added tests for synth constructor and fleshing out init tests

--- common/synth.test.js
+++ common/synth.test.js
@@ -8,13 +8,32 @@ describe("synth", () => {
       expect(Synth).toThrow();
     });
 
-    it("common voiceManager used if none provided", () => {
-      const test_synth = new Synth({
-        ctx: ctx_mock,
-        synthVoice: synthVoice_mock
+    describe("with synthVoice", () => {
+      it("default values set correctly", () => {
+        const test_synth = new Synth({
+          ctx: ctx_mock,
+          synthVoice: synthVoice_mock
+        });
+
+        // voiceManager used if none provided
+        expect(test_synth.voiceManager).not.toBeNull();
+        // four voices chosen as default
+        expect(test_synth.num_voices).toEqual(4);
+        // audioContext not null
+        expect(test_synth.ctx).not.toBeNull();
+        expect(test_synth.voices).toEqual([]);
       });
 
-      expect(test_synth.voiceManager).not.toBeNull();
+      it("synthVoice init is called", () => {
+        spyOn(synthVoice_mock, "init").and.callThrough();
+
+        new Synth({
+          ctx: ctx_mock,
+          synthVoice: synthVoice_mock
+        });
+
+        expect(synthVoice_mock.init).toHaveBeenCalled();
+      });
     });
   });
 
@@ -23,11 +42,27 @@ describe("synth", () => {
 
     beforeEach(() => {
       ctx_mock.reset();
-      synth = new Synth({ synthVoice: synthVoice_mock, ctx: ctx_mock });
+      synth = new Synth({
+        synthVoice: synthVoice_mock,
+        ctx: ctx_mock
+      });
     });
 
     describe("init", () => {
-      it("", () => {});
+      it("generateVoices called", () => {
+        spyOn(synth, "generateVoices").and.callThrough();
+
+        synth.init();
+
+        expect(synth.generateVoices).toHaveBeenCalled();
+      });
+
+      it("returns handlers", () => {
+        const result = synth.init();
+
+        expect(result.hasOwnProperty("noteOn")).toBeTruthy();
+        expect(result.hasOwnProperty("noteOff")).toBeTruthy();
+      });
     });
   });
 });
